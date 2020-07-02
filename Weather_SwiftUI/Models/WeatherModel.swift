02ed68b40712b58d27905aa24977a26e0365a0bd
Yoshinobu <Yoshinobu>
2020-07-02 14:41:42 +0900

    first commit

---
+++ Weather_SwiftUI/Models/WeatherModel.swift
@@ -0,0 +1,38 @@
+//
+//  Weather.swift
+//  Weather_SwiftUI
+//
+//  Created by 村尾慶伸 on 2020/07/02.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class WeatherModel {
+    let apiKey = "0e4203375ffd67b9320a9e4c2bdb8d7c"
+    var components: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "samples.openweathermap.org"
+        components.path = "/data/2.5/weather?q=London&appid=\(apiKey)"
+        return components
+    }
+
+    func fetchWeather() -> AnyPublisher<WeatherDataContainer, Error> {
+        return URLSession.shared.dataTaskPublisher(for: components.url!)
+            .map { $0.data }
+            .decode(type: WeatherDataContainer.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+
+}
+
+struct WeatherDataContainer: Decodable {
+    let name: String
+    let weather: Weather
+}
+
+struct Weather: Decodable {
+    let main: String
+}
---
+++ Weather_SwiftUI/Views/WeatherList.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather_SwiftUI
+//
+//  Created by 村尾慶伸 on 2020/07/02.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import SwiftUI
+
+struct WeatherList: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherList()
+    }
+}
